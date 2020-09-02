5ebb23f6f93d4432e2c73d4963c2714d116edbc1
Sam~Lubbers <lubbers.sc@gmail.com>
2020-09-02 14:51:10 +0200

    resolved merge conflicts with master, resolved linting issues and prettified code

--- src/store/modules/styles.js
+++ src/store/modules/styles.js
@@ -1,3 +1,19 @@
 export const state = {
   navbarHeight: "64px",
+  workingCircleColours: {
+    "Action & Logistics": "khaki-light",
+    "Arts in Action": "red-light",
+    Finance: "navy-light",
+    "Inclusion & Power": "yellow-light",
+    Integration: "yellow-light",
+    Legal: "navy-light",
+    "Media & Communications": "red-light",
+    "Outreach & Training": "purple-light",
+    "Political Strategy & Change": "green-light",
+    "Regenerative Culture": "orange-light",
+    Tech: "blue-light",
+    "Rebellion Support Circle": "pink-light",
+    "Coordination Circle": "purple-light",
+    "Future of Democracy": "green-light",
+  },
 };
